0611b92886ee6a092fac50dc27129490f00f044c
Will Bush <williamdcbush@gmail.com>
2020-02-16 11:49:53 +0000

    Footer component created.

--- cake-app/src/App.js
+++ cake-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
