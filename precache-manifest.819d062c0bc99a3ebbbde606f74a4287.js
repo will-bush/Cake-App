b497de0589713f21e6c3aa55fc59d76a9b0624da
Will Bush <williamdcbush@gmail.com>
2020-02-16 16:36:15 +0000

    Updates

---
+++ precache-manifest.819d062c0bc99a3ebbbde606f74a4287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb039d4cafc1388898e51981c1165a80",
+    "url": "/Cake-App/index.html"
+  },
+  {
+    "revision": "845867d4fbbd0a57af35",
+    "url": "/Cake-App/static/css/main.c34d872c.chunk.css"
+  },
+  {
+    "revision": "97639fd8ad85ef7e7ae9",
+    "url": "/Cake-App/static/js/2.373c2e20.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Cake-App/static/js/2.373c2e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845867d4fbbd0a57af35",
+    "url": "/Cake-App/static/js/main.251b27b7.chunk.js"
+  },
+  {
+    "revision": "ba3e80d348c4c51c3ffb",
+    "url": "/Cake-App/static/js/runtime-main.ca89f6e0.js"
+  }
+]);
