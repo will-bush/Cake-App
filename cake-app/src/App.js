e6882aaa458f33c6a8f9f5c618b6d54370d72bed
Will Bush <williamdcbush@gmail.com>
2020-02-16 13:04:12 +0000

    CakeForm Class Component created.

--- cake-app/src/App.js
+++ cake-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Cake from './Components/Cake';
+import CakeDetail from './Components/CakeDetail'
 import API from './API'
 
 class App extends React.Component {
@@ -32,6 +33,21 @@ class App extends React.Component {
     })
   }
 
+  // SHOWS A FORM FOR ADDING A NEW CAKE
+  showCakeForm = () => {
+    this.setState({
+      addCake: true
+    })
+    this.clearSelection();
+  }
+
+  // REMOVES A SINGLE CAKE FROM BEING "SELECTED" IN STATE
+  clearSelection = () => {
+    this.setState({
+      selected_cake: null
+    })
+  }
+
   render() {
   
   return (
@@ -39,8 +55,21 @@ class App extends React.Component {
       <Header title="Cake App"/>
         <div className="main-container">
           <div className="cakes">
+            {/* BUTTON ALLOWS USER TO ADD A NEW CAKE */}
+            <button onClick={() => this.showCakeForm()}>Add a New Cake</button>
             <h3>Select a Cake</h3>
             {this.state.cakes ? this.state.cakes.map(cake => <Cake cake={cake} selectCake={this.setSelectedCake} key={cake.id}/>) : null}
+            <div className="main">
+            {/* INSTRUCTS USER TO SELECT A CAKE OR ADD A NEW CAKE  */}
+            { !this.state.selected_cake && !this.state.addCake ? <h2>Please select a cake from the menu, or click "Add a New Cake".</h2>
+            : null }
+            {/* SHOWS A DETAILED VIEW OF THE SELECTED CAKE */}
+            {this.state.selected_cake ? <CakeDetail cake={this.state.selected_cake}/>
+            : null }
+            {/* SHOWS A FORM TO ADD A NEW CAKE */}
+            {this.state.addCake ?<CakeForm addCakeToList={this.addCakeToList}/>
+            : null }
+            </div>
           </div>
         </div>
       <Footer/>
---
+++ cake-app/src/Components/CakeForm.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import API from './API'
+
+class CakeForm extends React.Component {
+
+    state = {
+        name: "",
+        comment: "",
+        imageUrl: "",
+        yumFactor: 0
+    }
+
+    handleChange = event => this.setState({ [event.target.name]: event.target.value })
+        
+    handleSubmit = event => {
+        event.preventDefault();
+        API.postCake(this.state)
+        .then((resp) => this.props.addCakeToList(resp))
+    }
+
+    render() {
+        return (
+
+            <div>
+                <h1>FORM GOES HERE!</h1>
+                <form>
+                    <div className="form-group">
+                        <label>Cake Name: </label>
+                            <input onChange={this.handleChange} type="text" name="name" placeholder="Cake Name" value={this.state.name} /><br/>
+                        <label>Cake Comment: </label>
+                            <input onChange={this.handleChange} type="text" name="comment" placeholder="Cake Description" value={this.state.comment} /><br/>
+                        <label>Image URL: </label>
+                            <input onChange={this.handleChange} type="text" name="imageUrl" placeholder="Image URL" value={this.state.imageUrl} /><br/>
+                        <label>Yum Factor: </label>
+                            <select name="yumFactor" value={this.state.value} onChange={this.handleChange}>
+                                <option value="+1">1</option>
+                                <option value="+2">2</option>
+                                <option value="+3">3</option>
+                                <option value="+4">4</option>
+                                <option value="+5">5</option>
+                            </select>
+                        <input onClick={this.handleSubmit} type="submit" placeholder="Submit"/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+        
+export default CakeForm;
